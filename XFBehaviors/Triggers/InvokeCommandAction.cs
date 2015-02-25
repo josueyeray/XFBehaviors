ea7e7120d6ee3854d8b7eac379f79ab3723600a1
josueyeray <josueyeray@gmail.com>
2015-02-25 08:47:26 +0100

    Added NavigateToPageAction and InvokeCommandAction

---
+++ XFBehaviors/Triggers/InvokeCommandAction.cs
@@ -0,0 +1,64 @@
+﻿namespace XFBehaviors.Triggers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+
+    public class InvokeCommandAction : TriggerAction<View>
+    {
+        public InvokeCommandBinding CommandBinding { get; set; }
+
+        protected override void Invoke(View sender)
+        {
+            if (CommandBinding == null)
+            {
+                Debug.WriteLine("InvokeCommandAction.CommandBase is null");
+                return;
+            }
+            if (CommandBinding.Command == null)
+            {
+                Debug.WriteLine("InvokeCommandAction.CommandBase is null");
+                return;
+            }
+            if (!CommandBinding.Command.CanExecute(CommandBinding.CommandParameter))
+            {
+                Debug.WriteLine("InvokeCommandAction.CommandBase.Command.CanExecute returns false");
+                return;
+            }
+
+            CommandBinding.Command.Execute(CommandBinding.CommandParameter);
+        }
+    }
+
+    /// <summary>
+    /// Binding support for command and commandparameter.
+    /// </summary>
+    public class InvokeCommandBinding : BindableObject
+    {
+        private static readonly BindableProperty CommandProperty = BindableProperty.Create<InvokeCommandBinding, ICommand>(p => p.Command, null);
+        private static readonly BindableProperty CommandParameterProperty = BindableProperty.Create<InvokeCommandBinding, object>(p => p.CommandParameter, null);
+
+        /// <summary>
+        /// Command to be invoked.
+        /// </summary>
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        /// <summary>
+        /// Command parameter argument.
+        /// </summary>
+        public object CommandParameter
+        {
+            get { return GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+    }
+}
---
+++ XFBehaviors/Triggers/NavigateToPageAction.cs
@@ -0,0 +1,58 @@
+﻿namespace XFBehaviors.Triggers
+{
+    using System;
+    using System.Diagnostics;
+    using Xamarin.Forms;
+
+    /// <summary>
+    /// Allows you to navigate to TargetPage directly from XAML.
+    /// TargetPage need to be the full qualified name of the page class you
+    /// want to navigate to.
+    /// </summary>
+    public class NavigateToPageAction : TriggerAction<View>
+    {
+        private INavigation navService;
+
+        public NavigateToPageBinding TargetPageBinding { get; set; }
+
+        protected override async void Invoke(View sender)
+        {
+            this.navService = Application.Current.MainPage.Navigation;
+            if (this.navService == null)
+            {
+                Debug.WriteLine("Application.Current.MainPage.Navigation is null");
+                return;
+            }
+            if (TargetPageBinding == null)
+            {
+                Debug.WriteLine("NavigateToPageAction.TargetPageBinding is null");
+                return;
+            }
+            if (string.IsNullOrEmpty(TargetPageBinding.TargetPage))
+            {
+                Debug.WriteLine("NavigateToPageAction.TargetPageBinding.TargetPage is null");
+                return;
+            }
+            var targetPageType = Type.GetType(TargetPageBinding.TargetPage);
+            var targetPageInstance = (Page)Activator.CreateInstance(targetPageType);
+            await this.navService.PushAsync(targetPageInstance);
+        }
+    }
+
+    /// <summary>
+    /// Binding support for TargetPage
+    /// </summary>
+    public class NavigateToPageBinding : BindableObject
+    {
+        private static readonly BindableProperty TargetPageProperty = BindableProperty.Create<NavigateToPageBinding, string>(p => p.TargetPage, null);
+
+        /// <summary>
+        /// Full qualified name of the target page.
+        /// </summary>
+        public string TargetPage
+        {
+            get { return GetValue(TargetPageProperty).ToString(); }
+            set { SetValue(TargetPageProperty, value); }
+        }
+    }
+}
