641291d46300ed3030568061bf353a20f9004b78
josueyeray <josueyeray@gmail.com>
2015-02-25 19:32:15 +0100

    Added base animation behavior

---
+++ XFBehaviors/Base/AnimationBaseBehavior.cs
@@ -0,0 +1,54 @@
+﻿namespace XFBehaviors.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+    using XFBehaviors.Enums;
+
+    public class AnimationBaseBehavior : Behavior<View>
+    {
+        private static readonly BindableProperty DurationProperty = BindableProperty.Create<AnimationBaseBehavior, int>(a => a.Duration, 250);
+        private static readonly BindableProperty OnEventProperty = BindableProperty.Create<AnimationBaseBehavior, EventTypeEnumerator>(a => a.OnEvent, EventTypeEnumerator.Attached);
+        private static readonly BindableProperty WaitToEndProperty = BindableProperty.Create<AnimationBaseBehavior, bool>(a => a.WaitToEnd, false);
+        private static readonly BindableProperty EasingMethodProperty = BindableProperty.Create<AnimationBaseBehavior, Easing>(a => a.EasingMethod, Easing.Linear);
+
+        /// <summary>
+        /// Animation duration in milliseconds, default: 250ms
+        /// </summary>
+        public int Duration
+        {
+            get { return (int)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
+        }
+
+        /// <summary>
+        /// Launching event, default: Attached
+        /// </summary>
+        public EventTypeEnumerator OnEvent
+        {
+            get { return (EventTypeEnumerator)GetValue(OnEventProperty); }
+            set { SetValue(OnEventProperty, value); }
+        }
+
+        /// <summary>
+        /// Wait until animation ends, default: false
+        /// </summary>
+        public bool WaitToEnd
+        {
+            get { return (bool)GetValue(WaitToEndProperty); }
+            set { SetValue(WaitToEndProperty, value); }
+        }
+
+        /// <summary>
+        /// Easing function to apply to animation, default: Linear
+        /// </summary>
+        public Easing EasingMethod
+        {
+            get { return (Easing)GetValue(EasingMethodProperty); }
+            set { SetValue(EasingMethodProperty, value); }
+        }
+    }
+}
--- XFBehaviors/Behaviors/FadeInBehavior.cs
+++ XFBehaviors/Behaviors/FadeInBehavior.cs
@@ -1,25 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-using XFBehaviors.Enums;
-
-namespace XFBehaviors.Behaviors
+﻿namespace XFBehaviors.Behaviors
 {
-    public class FadeInBehavior : Behavior<View>
-    {
-        public EventTypeEnumerator OnEvent { get; set; }
-        protected override void OnAttachedTo(View bindable)
-        {
-            base.OnAttachedTo(bindable);
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+    using XFBehaviors.Base;
+    using XFBehaviors.Enums;
 
+    public class FadeInBehavior : AnimationBaseBehavior
+    {
+        private static readonly BindableProperty FinalOpacityProperty = BindableProperty.Create<FadeInBehavior, double>(a => a.FinalOpacity, 1);
+
+        /// <summary>
+        /// Final opacity, default: 1
+        /// </summary>
+        public double FinalOpacity
+        {
+            get { return (double)GetValue(FinalOpacityProperty); }
+            set { SetValue(FinalOpacityProperty, value); }
         }
 
-        protected override void OnDetachingFrom(View bindable)
+        protected override void OnAttachedTo(View element)
         {
-            base.OnDetachingFrom(bindable);
+            base.OnAttachedTo(element);
+        }
+
+        protected override void OnDetachingFrom(View element)
+        {
+            base.OnDetachingFrom(element);
+        }
+
+        private async Task DoAnimation(View element)
+        {
+            if (WaitToEnd)
+            {
+                await element.FadeTo(FinalOpacity, (uint)Duration, EasingMethod);
+            }
+            else
+            {
+                element.FadeTo(FinalOpacity, (uint)Duration, EasingMethod);
+            }
         }
     }
 }
