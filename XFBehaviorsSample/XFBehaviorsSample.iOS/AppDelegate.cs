9519b9b27943743e613b1d095385aeee12b4cc07
Josue Yeray Julian Ferreiro <josueyeray@gmail.com>
2016-01-18 14:23:24 +0100

    Added missing sln file and updated all projects to last stable Xamarin.Forms 2.0

--- XFBehaviorsSample/XFBehaviorsSample.iOS/AppDelegate.cs
+++ XFBehaviorsSample/XFBehaviorsSample.iOS/AppDelegate.cs
@@ -15,7 +15,7 @@ namespace XFBehaviorsSample.iOS
     {
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
+        //// method you should instantiate the window, load the UI into it and then make the window
         // visible.
         //
         // You have 17 seconds to return from this method, or iOS will terminate your application.
